b3f7b8f0b666e65ee802274d94fff50db9eedf00
nguyen duc <ducnguyen@rionlab.com>
2021-05-04 22:02:24 +0700

    first commit

--- src/sagas.js
+++ src/sagas.js
@@ -1,12 +1,10 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
 import axios from 'axios';
 
-// watcher saga: watches for actions dispatched to the store, starts worker saga
 export function* watcherSaga() {
   yield takeLatest('API_CALL_REQUEST', workerSaga);
 }
 
-// function that makes the api request and returns a Promise for response
 function fetchProduct() {
   return axios({
     method: 'GET',
@@ -14,16 +12,13 @@ function fetchProduct() {
   });
 }
 
-// worker saga: makes the api call when watcher saga sees the action
 function* workerSaga() {
   try {
     const response = yield call(fetchProduct);
     const data = response.data;
     console.log(data);
-    // dispatch a success action to the store with the new dog
     yield put({ type: 'API_CALL_SUCCESS', data });
   } catch (error) {
-    // dispatch a failure action to the store with the error
     yield put({ type: 'API_CALL_FAILURE', error });
   }
 }
